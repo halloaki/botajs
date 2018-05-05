a3ea80aee2699c8dcdbea1c522a7fb076eeb5578
Jacky Masset <halloakinator@gmail.com>
2018-05-05 10:06:50 +0200

    added water command

--- index.js
+++ index.js
@@ -59,7 +59,16 @@ bot.on("message", async message => {
   } else if (command === `${prefix}roll`) {
     message.channel.send("You rolled a " + Math.floor(math.random() * 6) + 1);
   } else if (command === `${prefix}cookie`){
-    message.channel.send(message.author.username + " gave a cookie to " + args[0])
+    message.channel.send(message.author.username + " gave a cookie to " + args[0]);
+  } else if (command === `${prefix}water`){
+    var phrases = [
+      'the shaman made it rain, hopefully it isn\'t acid rain like last time',
+      'you manage to scavenge water in the back of Miraz home <:water:438082707688259584>',
+      'sorry we don\'t have water for everybody today...',
+      'sorry maybe the shaman will make it rain tomorrow...',
+      'the well has dried up!'
+  ]
+    message.channel.send(phrases[math.floor(Math.random()*phrases.length)]);
   }
 });
 
