ad26917295a73607d5adcf449c2e87f0a7e20ad0
Jacky Masset <halloakinator@gmail.com>
2018-05-05 16:16:44 +0200

    removed dummy commands

--- index.js
+++ index.js
@@ -36,17 +36,4 @@ fs.readdir("./commands/", (err, files) => {
   });
 });
 
-/*
-  } else if (command === `${prefix}water`) {
-    var phrases = [
-      "the shaman made it rain, hopefully it isn't acid rain like last time",
-      "you manage to scavenge water in the back of Miraz home <:water:438082707688259584>",
-      "sorry we don't have water for everybody today...",
-      "sorry maybe the shaman will make it rain tomorrow...",
-      "the well has dried up!"
-    ];
-    message.channel.send(phrases[Math.floor(Math.random() * phrases.length)]);
-  }
-});
-*/
 client.login(token);
