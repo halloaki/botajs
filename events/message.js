14245ce781a8d3af5159a7f1f103ceae1fd0e281
Jacky Masset <halloakinator@gmail.com>
2018-05-05 21:03:20 +0200

    attempting bugfix

--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
-module.exports = (client, message) => {
+exports.run = (client, message) => {
   const prefix = client.config.prefix;
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
