70804e7f696177210b19f479c6ac54b4116dac81
Jacky Masset <halloakinator@gmail.com>
2018-05-22 13:27:09 +0200

    removed cola

--- events/message.js
+++ events/message.js
@@ -4,7 +4,6 @@ exports.run = (client, message) => {
   if (message.channel.type === "dm") return;
   if (message.content.includes("shro")) message.channel.send("It's SCHRÖ!!!");
   if (message.content.includes("sota") || message.content.includes("Sota")) message.channel.send("It's SotA!!!");
-  if (message.content.includes("cola")) message.channel.send("It's Cola!!!");
   if (message.content.indexOf(prefix) !== 0) return;
 
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
