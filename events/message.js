45f98504c52bf247d8857220b791b787b10a99b0
Jacky Masset <halloakinator@gmail.com>
2018-05-22 13:07:26 +0200

    Addded shroe

--- events/message.js
+++ events/message.js
@@ -2,6 +2,7 @@ exports.run = (client, message) => {
   const prefix = client.config.prefix;
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
+  if (message.content.indexOf("shro")) message.channel.send("It's SCHRÖ!!!");
   if (message.content.indexOf(prefix) !== 0) return;
 
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
