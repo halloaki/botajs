a31503ceaa400b4f7a82737bbf472f7fdd4c7555
Jacky Masset <halloakinator@gmail.com>
2018-05-30 22:28:51 +0200

    Major bugfix

--- commands/convertTWA.js
+++
@@ -1,8 +0,0 @@
-exports.run = (client, message, args) => {
-    try {
-        var euro = args[0] * 0.029;
-        message.channel.send(`It should be worth around ${euro} Euros`);
-    } catch (error) {
-        message.channel.send(`Sorry, I can't convert that.`);
-    }
-};
